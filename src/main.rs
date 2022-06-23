ff9c7582af6106cbcb69b348deab42f3dd9f2f63
Nate Wilson <nsw.wilson@gmail.com>
2022-06-22 22:28:19 -0400

    adding loading of assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+mod resources;
+
+mod prelude {
+    pub use bevy::prelude::*;
+    pub const TILE_SIZE: u8 = 8;
+    pub use crate::resources::*;
+}
+
+use prelude::*;
+
+fn setup(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let image = assets.load("fontfile.png");
+    let atlas = TextureAtlas::from_grid(image, Vec2::splat(TILE_SIZE as f32), 16, 16);
+    let atlas_handle = texture_atlases.add(atlas);
+
+    commands.insert_resource(SpriteSheet {
+        atlas: atlas_handle,
+    });
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Rust Roguelike".to_string(),
+            ..Default::default()
+        })
+        .add_startup_system_to_stage(StartupStage::PreStartup, setup.label("setup"))
+        .add_plugins(DefaultPlugins)
+        .run();
 }
---
+++ src/resources.rs
@@ -0,0 +1,5 @@
+use crate::prelude::*;
+
+pub struct SpriteSheet {
+    pub atlas: Handle<TextureAtlas>,
+}
